a2892c7c8b48da706384934c5b81092a3fffbd25
Julian Maximiliano González <155547378+jgonzalez-clever@users.noreply.github.com>
2024-05-31 20:04:19 -0300

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Install dependencies
 pip install --no-cache-dir -r requirements.txt
+
+# Run test
 python test.py
 pytest test.py --cov --cov-report xml:coverage/test-reporter.xml
+
+# Coverage percentages
+coverage=$(grep -oP 'Statements.*?(\d+(?:\.\d+)?)%' coverage/test-reporter.xml | grep -oP '\d+(?:\.\d+)?')
+
+if [[ -z "$coverage" ]]; then
+  echo "No se encontró 'Statements' en coverage/test-reporter.xml"
+  # Asigna un valor por defecto o maneja el error según sea necesario
+  coverage="0.0"
+else
+  echo "Cobertura encontrada: $coverage%"
+fi
